24fbb63cc4d8261b498caf6e9f50d8380e7bd6bc
RomanZujev <roman.zujev@gmail.com>
2022-03-30 19:57:38 +0300

    added taskDisplay and input elements

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import "./App.css";
+import { useState } from "react";
 import Month from "./components/Month.js";
+import TaskDataDisplay from "./components/TaskDataDisplay.js";
+import Input from "./components/Input.js";
 function App() {
   let date = new Date();
+  const [taskData, setTaskData] = useState({});
   return (
     <div className="App">
       <Month date={date} />
+      <TaskDataDisplay
+        taskData={{ title: "one", time: "two", details: "three" }}
+      />
+      <Input />
     </div>
   );
 }
