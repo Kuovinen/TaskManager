d618e66e931e19d233547edc47551c82893bc689
RomanZujev <roman.zujev@gmail.com>
2022-03-31 15:34:12 +0300

    month component now decides current day and sends taskData to day components

--- src/components/Month.js
+++ src/components/Month.js
@@ -1,23 +1,49 @@
 import Day from "./Day.js";
+import Weekday from "./Weekday.js";
+import Task from "./Task.js";
+import taskList from "./taskList.js";
 import { useEffect, useState } from "react";
 import getMonthString, { createYearData, checkIfWeekend } from "./functions.js";
 
 export default function Month(props) {
   let year = props.date.getFullYear();
   let yearObject = createYearData(year);
+  let weekdays = [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday",
+  ];
+
   //Create array fillied with day components, adding a style prop to weekends
   function constructMonth(number) {
     let days = yearObject.months[number].map((element, index) => {
       let weekend = false;
-      if ((index + 2) % 7 == 0 || (index + 1) % 7 == 0) {
-        console.log(index);
+      let current = false;
+
+      if ((index + 2) % 7 === 0 || (index + 1) % 7 === 0) {
         weekend = true;
       }
+      if (
+        element != null &&
+        props.date.getDate() === element.date &&
+        props.date.getMonth() === number
+      ) {
+        current = true;
+      }
+
       return (
         <Day
           key={index + number}
-          day={element == null ? null : element.date}
+          day={element === null ? null : element.date}
+          month={number}
           weekend={weekend}
+          current={current}
+          taskData={props.taskData}
+          setTaskData={props.setTaskData}
         />
       );
     });
@@ -65,6 +91,11 @@ export default function Month(props) {
         <button className="chngM" onClick={nextMonth}>
           {">>"}
         </button>
+      </div>
+      <div className="weekdays">
+        {weekdays.map((element) => (
+          <Weekday key={element} day={element} />
+        ))}
       </div>
       <div className="month">{displayedMonth}</div>
     </section>
