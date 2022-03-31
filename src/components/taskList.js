085afe49bc101aaa83f33e6b76378af749e249b6
RomanZujev <roman.zujev@gmail.com>
2022-03-31 15:31:44 +0300

    temporary hardcoded task Data

---
+++ src/components/taskList.js
@@ -0,0 +1,11 @@
+let taskList = {
+  2022: {
+    2: {
+      30: [
+        { title: "dog", time: "13:00", details: "wash dog!" },
+        { title: "cat", time: "09:00", details: "feed cat" },
+      ],
+    },
+  },
+};
+export default taskList;
