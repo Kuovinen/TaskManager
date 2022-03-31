d3ca4030f0707fee5ddaf7af7acdd32056a6f47e
RomanZujev <roman.zujev@gmail.com>
2022-03-31 15:33:05 +0300

    day component now chooses border color dependent on current day and assigns tasks

--- src/components/Day.js
+++ src/components/Day.js
@@ -1,17 +1,42 @@
+import Task from "./Task.js";
+import taskList from "./taskList.js";
+
 export default function Day(props) {
+  let background = "none";
+  let border = "0.1rem solid var(--cp-Black)";
+  let tasks = [];
+  if (props.weekend) {
+    background = "var(--cp-lGrey)";
+  }
+  if (props.current) {
+    border = "0.2rem solid var(--cp-Purple)";
+  }
+  //create tasks
+  if (
+    props.day &&
+    taskList[2022][props.month] &&
+    taskList[2022][props.month][props.day]
+  ) {
+    tasks = taskList[2022][props.month][props.day].map((element, index) => (
+      <Task
+        key={index}
+        title={element.title}
+        time={element.time}
+        details={element.details}
+        setTaskData={props.setTaskData}
+      />
+    ));
+  }
+
   return (
-    <div
-      className="day"
-      style={props.weekend ? { background: "var(--cp-lGrey)" } : null}
-    >
+    <div className="day" style={{ background: background, border: border }}>
       <div
         className="date"
-        style={props.day == null ? null : { background: "var(--cp-Black)" }}
+        style={props.day === null ? null : { background: "var(--cp-Black)" }}
       >
         {props.day}
       </div>
-      {props.weekday}
-      <div className="tasks"></div>
+      <div className="dayTasks">{tasks}</div>
     </div>
   );
 }
