88f35686462b77e00ee25829def4950a5a3d3f19
RomanZujev <roman.zujev@gmail.com>
2022-04-01 19:22:21 +0300

    Added taskList states to Input and Month components to allow task appearance on calendar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import "./App.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Month from "./components/Month.js";
 import TaskDataDisplay from "./components/TaskDataDisplay.js";
 import Input from "./components/Input.js";
 import SelectedDay from "./components/SelectedDay.js";
+import tasks from "./components/taskList.js";
 function App() {
   let date = new Date();
   const [taskData, setTaskData] = useState({});
@@ -12,6 +13,8 @@ function App() {
     month: null,
     tasks: [{ title: null, time: null, details: null }],
   });
+  const [taskList, setTaskList] = useState(tasks);
+
   return (
     <div className="App">
       <Month
@@ -19,11 +22,12 @@ function App() {
         setSelectedDay={setSelectedDay}
         taskData={taskData}
         setTaskData={setTaskData}
+        taskList={taskList}
       />
       <div className="workspace">
         <SelectedDay setTaskData={setTaskData} data={selectedDay} />
         <TaskDataDisplay taskData={{ ...taskData }} />
-        <Input />
+        <Input setTaskList={setTaskList} taskList={taskList} />
       </div>
     </div>
   );
