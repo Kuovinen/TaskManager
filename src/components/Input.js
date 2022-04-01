dd3201369e09477bcd73a2ca0e69bdb14fe8bb51
RomanZujev <roman.zujev@gmail.com>
2022-04-01 19:25:06 +0300

    added hardcoded task that gets added to calendar through IMMER library function update

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,11 +1,29 @@
-export default function Input() {
-  function saveData() {
-    console.log("Save data to file");
+import produce from "immer";
+import React from "react";
+export default function Input(props) {
+  function saveData(event) {
+    event.preventDefault();
+    // if there is text in input field (then it's "trufy")
+    // make a message element with the text value
+    // inputTXT &&
+
+    props.setTaskList(
+      produce((draft) => {
+        draft[2022][3] = {
+          ...draft[2022][3],
+          2: [{ title: "DOG WASHING", time: "13:00", details: "Lorem" }],
+        };
+      })
+    );
   }
 
   return (
     <div className="inputForm">
-      <form>
+      <form
+        onSubmit={(event) => {
+          saveData(event);
+        }}
+      >
         <input className="iInput" placeholder="TITLE"></input>
         <input className="iInput" placeholder="TIME"></input>
         <textarea className="iDetails" placeholder="DETAILS"></textarea>
