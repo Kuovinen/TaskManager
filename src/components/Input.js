76c1a2578559995bbfa6312bd94dce7f9238140b
RomanZujev <roman.zujev@gmail.com>
2022-04-03 15:50:07 +0300

    fixed bug where unselected day or empty data bundle would crash the page by adding if condition in the SaveData function

--- src/components/Input.js
+++ src/components/Input.js
@@ -10,37 +10,45 @@ export default function Input(props) {
     //needs proper IF conditioning
     props.setTaskList(
       produce((draft) => {
-        //decide if the day already has tasks assigned to it
-        //if so - spread them out , add a new one and assign all to the value
-        //if not, simply assign the new one to the value
-        let newDayContent = draft[2022][props.month][props.day]
-          ? [
-              ...draft[2022][props.month][props.day],
-              {
-                title: event.target[0].value,
-                time: event.target[1].value,
-                details: event.target[2].value,
-              },
-            ]
-          : [
-              {
-                title: event.target[0].value,
-                time: event.target[1].value,
-                details: event.target[2].value,
-              },
-            ];
-        //update the month data by spreading it and adding content of the
-        //set up day data
-        draft[2022][props.month] = {
-          ...draft[2022][props.month],
-          [props.day]: newDayContent,
-        };
+        //if there is at least SOME data to save and a day is selected
+        if (
+          (event.target[0].value ||
+            event.target[1].value ||
+            event.target[2].value) &&
+          props.day
+        ) {
+          //decide if the day already has tasks assigned to it
+          //if so - spread them out , add a new one and assign all to the value
+          //if not, simply assign the new one to the value
+          let newDayContent = draft[2022][props.month][props.day]
+            ? [
+                ...draft[2022][props.month][props.day],
+                {
+                  title: event.target[0].value,
+                  time: event.target[1].value,
+                  details: event.target[2].value,
+                },
+              ]
+            : [
+                {
+                  title: event.target[0].value,
+                  time: event.target[1].value,
+                  details: event.target[2].value,
+                },
+              ];
+          //update the month data by spreading it and adding content of the
+          //set up day data
+          draft[2022][props.month] = {
+            ...draft[2022][props.month],
+            [props.day]: newDayContent,
+          };
+        }
       })
     );
-
-    /****************************************************************************Add a save feature function here*/
   }
-
+  function deleteData() {
+    console.log(props.taskList[2022][props.month][props.day]);
+  }
   return (
     <div className="inputForm">
       <form
@@ -49,7 +57,7 @@ export default function Input(props) {
         }}
       >
         <input className="iInput" placeholder="TITLE"></input>
-        <input className="iInput" placeholder="TIME"></input>
+        <input className="iInput" type="time"></input>
         <textarea className="iDetails" placeholder="DETAILS"></textarea>
         <input className="submitButton" type="submit" value="save"></input>
       </form>
